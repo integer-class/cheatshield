755fb1ce22ab7b812e45033027eafc160e17186e
zharsuke <alazharisqi@gmail.com>
2024-10-31 13:22:38 +0700

    feat: manage quiz

--- cheatshield-admin/app/Filament/Resources/UserResource.php
+++ cheatshield-admin/app/Filament/Resources/UserResource.php
@@ -33,6 +33,7 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
+                Tables\Columns\TextColumn::make('id')->searchable(),
                 Tables\Columns\TextColumn::make('name')->searchable(),
                 Tables\Columns\TextColumn::make('email'),
             ])
