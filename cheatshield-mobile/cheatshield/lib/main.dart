e4b049f8f75e6a1dde90295d703a0575311aaf75
zharsuke <alazharisqi@gmail.com>
2024-11-27 23:18:45 +0700

    feat: split routes

--- cheatshield-mobile/cheatshield/lib/main.dart
+++ cheatshield-mobile/cheatshield/lib/main.dart
@@ -1,24 +1,6 @@
-import 'package:cheatshield/screens/camera_screen.dart';
-import 'package:cheatshield/screens/home_screen.dart';
-import 'package:cheatshield/screens/login_screen.dart';
-import 'package:cheatshield/screens/profile_screen.dart';
-import 'package:cheatshield/screens/quiz_screen.dart';
+import 'package:cheatshield/routes/router.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
-import 'package:go_router/go_router.dart';
-
-// GoRouter configuration
-final _router = GoRouter(
-  routes: [
-    GoRoute(path: '/', builder: (context, state) => const LoginScreen()),
-    GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
-    GoRoute(path: '/quiz', builder: (context, state) => const QuizScreen()),
-    GoRoute(path: '/camera', builder: (context, state) => const CameraScreen()),
-    GoRoute(
-        path: '/profile', builder: (context, state) => const ProfileScreen()),
-  ],
-);
 
 void main() {
   runApp(const MyApp());
@@ -36,7 +18,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         fontFamily: GoogleFonts.montserrat().fontFamily,
       ),
-      routerConfig: _router,
+      routerConfig: appRouter,
     );
   }
 }
---
+++ cheatshield-mobile/cheatshield/lib/routes/router.dart
@@ -0,0 +1,17 @@
+import 'package:cheatshield/screens/camera_screen.dart';
+import 'package:cheatshield/screens/home_screen.dart';
+import 'package:cheatshield/screens/login_screen.dart';
+import 'package:cheatshield/screens/profile_screen.dart';
+import 'package:cheatshield/screens/quiz_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter appRouter = GoRouter(
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => const LoginScreen()),
+    GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
+    GoRoute(path: '/quiz', builder: (context, state) => const QuizScreen()),
+    GoRoute(path: '/camera', builder: (context, state) => const CameraScreen()),
+    GoRoute(
+        path: '/profile', builder: (context, state) => const ProfileScreen()),
+  ],
+);
