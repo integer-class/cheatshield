35ce4771aa49c305e870c6135676c51e8a075563
zharsuke <alazharisqi@gmail.com>
2024-12-03 19:23:33 +0700

    feat: add logout service

--- cheatshield-mobile/cheatshield/lib/services/web/auth_service.dart
+++ cheatshield-mobile/cheatshield/lib/services/web/auth_service.dart
@@ -49,4 +49,44 @@ class AuthService {
       }
     }
   }
+
+  // logout
+  Future<String?> logout(String token) async {
+    try {
+      final response = await _dio.post(
+        '$baseUrl/auth/logout',
+        options: Options(
+          headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $token',
+          },
+          followRedirects: false,
+          validateStatus: (status) {
+            return status! < 500; // Accept status codes less than 500
+          },
+        ),
+      );
+
+      // if response status code is 200, return the message
+      if (response.statusCode == 200) {
+        return response.data['message'];
+      } else {
+        // if response status code is not 200, return the error message
+        return response.data['message'];
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        // Handle the error based on the status code
+        if (e.response?.statusCode == 302) {
+          return 'Redirection: further action needs to be taken in order to complete the request';
+        } else {
+          return 'Error: ${e.response?.statusCode} - ${e.response?.statusMessage}';
+        }
+      } else {
+        // Handle other errors such as network issues
+        return 'Network error: ${e.message}';
+      }
+    }
+  }
 }
