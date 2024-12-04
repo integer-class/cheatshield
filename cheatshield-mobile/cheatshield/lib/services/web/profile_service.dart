136bc8c47f61f37f13dc75f72e9429ba49967b65
zharsuke <alazharisqi@gmail.com>
2024-12-05 02:39:03 +0700

    feat: add update profile service

--- cheatshield-mobile/cheatshield/lib/services/web/profile_service.dart
+++ cheatshield-mobile/cheatshield/lib/services/web/profile_service.dart
@@ -5,7 +5,7 @@ class ProfileService {
 
   ProfileService(this._dio);
 
-  final String baseUrl = 'http://192.168.1.8:80/api/v1';
+  final String baseUrl = 'http://192.168.254.146:80/api/v1';
 
   Future<Map<String, dynamic>> getProfile(String token) async {
     try {
@@ -31,6 +31,48 @@ class ProfileService {
       } else {
         return {
           'error': 'Failed to load profile, status code: ${response.statusCode}'
+        };
+      }
+    } on DioException catch (e) {
+      if (e.response != null) {
+        return {
+          'error':
+              'Error: ${e.response?.statusCode} - ${e.response?.statusMessage}'
+        };
+      } else {
+        return {'error': 'Network error: ${e.message}'};
+      }
+    }
+  }
+
+  // updateProfile method
+  Future<Map<String, dynamic>> updateProfile(
+      String token, Map<String, dynamic> data) async {
+    try {
+      final response = await _dio.put(
+        '$baseUrl/user',
+        data: data,
+        options: Options(
+          headers: {
+            'accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $token',
+          },
+          followRedirects: false,
+          validateStatus: (status) {
+            return status! < 500; // Accept status codes less than 500
+          },
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        return response.data ?? {}; // Ensure non-null response
+      } else if (response.statusCode == 401) {
+        return {'error': 'Unauthorized: Invalid or expired token'};
+      } else {
+        return {
+          'error':
+              'Failed to update profile, status code: ${response.statusCode}'
         };
       }
     } on DioException catch (e) {
