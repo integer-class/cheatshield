4734a540de6c0e7ca4ea61aaca9609a93eec8ea3
zharsuke <alazharisqi@gmail.com>
2024-12-11 19:08:41 +0700

    fix: adjust provider json response with model

--- cheatshield-mobile/cheatshield/lib/providers/web/quiz_provider.dart
+++ cheatshield-mobile/cheatshield/lib/providers/web/quiz_provider.dart
@@ -1,45 +1,30 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cheatshield/services/web/quiz_service.dart';
+import 'package:cheatshield/models/quiz_model.dart';
 
-final quizProvider =
-    StateNotifierProvider<QuizNotifier, Map<String, dynamic>?>((ref) {
+final quizProvider = StateNotifierProvider<QuizNotifier, QuizResponse?>((ref) {
   return QuizNotifier();
 });
 
-class QuizNotifier extends StateNotifier<Map<String, dynamic>?> {
+class QuizNotifier extends StateNotifier<QuizResponse?> {
   QuizNotifier() : super(null);
 
   final QuizService _quizService = QuizService();
 
-  Future<Map<String, dynamic>?> joinQuiz(String code, String token) async {
-    final response = await _quizService.joinQuiz(code, token);
-    state = response;
-
-    if (response != null) {
-      // Mendapatkan daftar pertanyaan
-      final questions = response['quiz']?['quiz']?['questions'];
-      if (questions != null && questions is List) {
-        for (var question in questions) {
-          print('Question: ${question['content']}');
-
-          // Mendapatkan daftar jawaban
-          final answers = question['answers'];
-          if (answers != null && answers is List) {
-            for (var answer in answers) {
-              print(
-                  'Answer: ${answer['content']}, Correct: ${answer['is_correct']}');
-            }
-          } else {
-            print('No answers available for this question.');
-          }
+  Future<void> joinQuiz(String code, String token) async {
+    final quizResponse = await _quizService.joinQuiz(code, token);
+    if (quizResponse != null) {
+      state = quizResponse;
+      print('Message: ${quizResponse.message}');
+      print('Quiz Title: ${quizResponse.quizSession.title}');
+      for (var question in quizResponse.quizSession.quiz.questions) {
+        print('Question: ${question.content}');
+        for (var answer in question.answers) {
+          print('Answer: ${answer.content}, Correct: ${answer.isCorrect}');
         }
-      } else {
-        print('No questions available in the quiz.');
       }
     } else {
       print('Failed to join the quiz.');
     }
-
-    return response;
   }
 }
