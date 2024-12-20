541dfa686122720c77dad719118b1e20a775abbf
elianiva <dicha.arkana03@gmail.com>
2024-12-20 14:34:16 +0700

    fix: force https

--- cheatshield-admin/app/Providers/AppServiceProvider.php
+++ cheatshield-admin/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\PersonalAccessToken;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Sanctum\Sanctum;
 
@@ -22,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
